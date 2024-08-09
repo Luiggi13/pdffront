7b63424cdb8326565ba2151119dac3d57365f48b
Christian Llansola <christian.llansola@mail.schwarz>
2024-08-09 08:19:19 +0200

    new table component

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
 
 import App from './App.vue';
 import router from './router';
@@ -10,7 +12,10 @@ import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export async function setupApp() {
-  const app = createApp(App).use(createPinia().use(piniaPluginPersistedstate)).use(router);
+  const app = createApp(App)
+    .use(createPinia().use(piniaPluginPersistedstate))
+    .component('EasyDataTable', Vue3EasyDataTable)
+    .use(router);
   app.mount('#app');
   return app;
 }
