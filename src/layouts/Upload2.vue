f991a38db10a0238879879773f66015d237b6f1e
Christian Llansola <christian.llansola@mail.schwarz>
2024-10-26 16:15:45 +0200

    link in new tab

--- src/layouts/Upload2.vue
+++ src/layouts/Upload2.vue
@@ -26,7 +26,7 @@
         @click="uploadFile()">Upload</button>
       </div>
       <div v-else> Loading ... </div>
-      <a v-if="lastLink.length > 0" :href="lastLink" :download="lastLink" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mx-auto">Descargar archivo</a>
+      <a v-if="lastLink.length > 0" :href="lastLink" :download="lastLink" target="_blank" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mx-auto">Descargar archivo</a>
   </div>
 </template>
 
