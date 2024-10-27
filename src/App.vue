bf10dcc64fe2a119a763c7a6c5ec34451c5abd36
Christian Llansola <christian.llansola@mail.schwarz>
2024-10-27 11:23:10 +0100

    Update code, max file size and overlay

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import { useAppStore } from './stores/appStore';
+const appStore = useAppStore();
 </script>
 <template>
   <RouterView />
